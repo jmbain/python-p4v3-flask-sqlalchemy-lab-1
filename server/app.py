b88c1747669851185bcc047a9a189f592456c349
Josh Bain <joshuambain@gmail.com>
2024-09-16 14:56:59 -0400

    complete flask sqlalchemy lab 1

--- server/app.py
+++ server/app.py
@@ -21,7 +21,24 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def eq_by_id(id):
+    eq = Earthquake.query.filter(Earthquake.id == id).first()
+    if not eq:
+        return {"message": f"Earthquake {id} not found."}, 404
+    return eq.to_dict(), 200
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def eqs_by_mag(magnitude):
+    eqs = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    # result = []
+    # for eq in eqs:
+    #     result.append(eq.to_dict())
+
+    return {
+        'count': len(eqs),
+        'quakes': [eq.to_dict() for eq in eqs]
+    }
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
